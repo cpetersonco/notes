9706734dce0f66e1af35e90dcbee803d7a4ecc4f
Christian Peterson <cdpeterson@email.arizona.edu>
2020-08-13 18:02:11 -0700

    refactor into hooks

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
@@ -6,69 +6,54 @@ import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
 
-class Form extends React.Component {
-    constructor(props) {
-        super(props)
+const NoteForm = (props) => {
+    const [content, setContentText] = useState('')
 
-        this.state = {
-            content: '',
-        }
-
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault()
-        this.props.onAdd(this.state.content)
-    }
+        props.onAdd(content)
+    };
 
-    handleChange(event) {
-        this.setState({
-            content: event.target.value,
-        })
-    }
-
-    render() {
-        return (
-            <Container maxWidth='sm'>
-                <h1>Save a Note</h1>
-                <Paper>
-                    <Box pr={2} pb={1}>
-                        <Grid item>
-                            <TextField
-                                id='outlined-textarea'
-                                label='New Note'
-                                placeholder='Complete your note...'
-                                style={{ margin: 8 }}
-                                multiline
-                                fullWidth
-                                rows={6}
-                                variant='outlined'
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                value={this.state.content}
-                                onChange={this.handleChange}
-                            />
-                        </Grid>
-                        <Grid
-                            item
-                            container
-                            direction='row'
-                            justify='flex-end'
-                            alignItems='baseline'>
-                            <Button
-                                onClick={this.handleSubmit}
-                                variant='contained'>
-                                Save
-                            </Button>
-                        </Grid>
-                    </Box>
-                </Paper>
-            </Container>
-        )
-    }
+    return (
+        <Container maxWidth='sm'>
+            <h1>Save a note</h1>
+            <Paper>
+                <Box pr={2} pb={1}>
+                    <Grid item>
+                        <TextField
+                            id='outlined-textarea'
+                            label='New Note'
+                            placeholder='Complete your note...'
+                            style={{ margin: 8 }}
+                            multiline
+                            fullWidth
+                            rows={6}
+                            variant='outlined'
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            value={content}
+                            onChange={(e) => {
+                                setContentText(e.target.value);
+                            }}
+                        />
+                    </Grid>
+                    <Grid
+                        item
+                        container
+                        direction='row'
+                        justify='flex-end'
+                        alignItems='baseline'>
+                        <Button
+                            onClick={handleSubmit}
+                            variant='contained'>
+                            Save
+                        </Button>
+                    </Grid>
+                </Box>
+            </Paper>
+        </Container>
+    )
 }
 
-export default Form
+export default NoteForm
