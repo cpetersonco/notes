b17980860132a749c98a4a41f3c849c25f0c6967
Christian Peterson <cdpeterson@email.arizona.edu>
2020-08-20 19:18:04 -0700

    refactor to components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Home from './Home'
-import Login from './Login'
-import SignUp from './SignUp'
+import Home from './components/HomePage'
+import Login from './components/LoginPage'
+import SignUp from './components/SignUpPage'
 
 const App = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Home} />
-                <Route path='/login' exact component={Login} />
-                <Route path='/signup' exact component={SignUp} />
+                <Route path='/login' component={Login} />
+                <Route path='/signup' component={SignUp} />
                 <Route path='/' render={() => <div>404</div>} />
             </Switch>
         </BrowserRouter>
