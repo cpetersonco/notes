3ce13b5f864671289aae4dac3a88fc7c92d2a5a5
Christian Peterson <cdpeterson@email.arizona.edu>
2020-12-30 19:53:59 -0700

    remove unnecessary button

--- src/components/ButtonAppBar.js
+++ src/components/ButtonAppBar.js
@@ -4,8 +4,6 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
 import firebase from '../api/firebase'
 
 const useStyles = makeStyles((theme) => ({
@@ -37,13 +35,6 @@ const ButtonAppBar = ({ history }) => {
         <div className={classes.root}>
             <AppBar position='static'>
                 <Toolbar>
-                    <IconButton
-                        edge='start'
-                        className={classes.menuButton}
-                        color='inherit'
-                        aria-label='menu'>
-                        <MenuIcon />
-                    </IconButton>
                     <Typography variant='h6' className={classes.title}>
                         Notes
                     </Typography>
@@ -53,7 +44,8 @@ const ButtonAppBar = ({ history }) => {
                                 Logout
                             </Button>
                             : <div>
-                                <Button color='inherit' onClick={() => history.push('/signup')}>Sign Up
+                                <Button color='inherit' onClick={() => history.push('/signup')}>
+                                    Sign Up
                                 </Button>
                                 <Button color='inherit' onClick={() => history.push('/login')}>
                                     Login
