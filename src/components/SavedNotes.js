e3723e63658b12057a744620b3ff63098c60f5e1
Christian Peterson <cdpeterson@email.arizona.edu>
2020-12-30 21:51:28 -0700

    add stream from firestore

--- src/components/SavedNotes.js
+++ src/components/SavedNotes.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Note from './Note'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
+import { streamNoteItems } from '../api/firebase'
 
 const SavedNotes = (props) => {
+    const [savedNotes, setSavedNotes] = useState([])
+
+    useEffect(() => {
+        const unsubscribe = streamNoteItems({
+            next: querySnapshot => {
+                const updatedNoteItems = querySnapshot.docs.map(docSnapshot => docSnapshot.data())
+                setSavedNotes(updatedNoteItems)
+            },
+            error: () => console.log('Get note item failed.')
+        })
+        return () => unsubscribe() // unsubscribing from the listener when the component is unmounting.
+    }, [])
+
     return (
         <Container maxWidth='sm'>
             <h1>Saved Notes</h1>
             <Grid container spacing={2}>
-                {props.savedNotes.map((note, index) => {
-                    return <Note key={index} content={note} />
+                {savedNotes.map((note, index) => {
+                    return <Note key={index} content={note.content} />
                 })}
             </Grid>
         </Container>
